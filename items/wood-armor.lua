edab2c1400e8c76ac45fa3678715bf9e140bd62e
gaby3xD <82486763+gaby3xD@users.noreply.github.com>
2021-04-30 14:15:38 +0800

    created item: Wood Armor

---
+++ items/wood-armor.lua
@@ -0,0 +1,10 @@
+local item = loadstring("https://raw.githubusercontent.com/gaby3xD/raison-detre/main/classes/item.lua")()
+
+local self = item.create(
+	"Wood Armor",
+	"A basic wood armor",
+	1,
+	true
+)
+
+return self
