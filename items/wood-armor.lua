24ca3e9217f12592e8197bdaf7dd8981ef525b6d
gaby3xD <82486763+gaby3xD@users.noreply.github.com>
2021-04-30 14:24:58 +0800

    Update wood-armor.lua

--- items/wood-armor.lua
+++ items/wood-armor.lua
@@ -1,4 +1,7 @@
-local item = loadstring("https://raw.githubusercontent.com/gaby3xD/raison-detre/main/classes/item.lua")()
+local HttpService = game:GetService("HttpService")
+local repo = HttpService:GetAsync("https://raw.githubusercontent.com/gaby3xD/raison-detre/main/classes/item.lua")
+
+local item = loadstring(repo)()
 
 local self = item.create(
 	"Wood Armor",
